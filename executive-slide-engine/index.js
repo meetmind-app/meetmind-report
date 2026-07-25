2d66586bc85f5429aebe6408a90a9c6d6619e691
zarubinscky <zarubinscky@gmail.com>
2026-07-25 21:51:44 +0300

    Update index.js

--- executive-slide-engine/index.js
+++ executive-slide-engine/index.js
@@ -1,1 +1,13 @@
+/**
+ * Executive Slide Engine
+ * Public API
+ *
+ * This file is the only entry point of the engine.
+ * The Web Report must interact only with this module.
+ */
 
+export async function generate(reportJson, options = {}) {
+    throw new Error(
+        "Executive Slide Engine is not implemented yet."
+    );
+}
