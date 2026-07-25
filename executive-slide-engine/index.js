2a1933d781dc375a628c700808fdddb39e1ed4dd
zarubinscky <zarubinscky@gmail.com>
2026-07-25 22:08:25 +0300

    Update index.js

--- executive-slide-engine/index.js
+++ executive-slide-engine/index.js
@@ -1,6 +1,7 @@
 import { normalizeReport } from "./schema.js";
 import { getEnabledBlocks } from "./block-registry.js";
 import { createLayout } from "./layout-engine.js";
+import { renderPdf } from "./pdf-renderer.js";
 
 export async function generate(reportJson, options = {}) {
     const report = normalizeReport(reportJson);
@@ -9,9 +10,9 @@ export async function generate(reportJson, options = {}) {
 
     const layout = createLayout(blocks);
 
-    return {
+    return renderPdf({
         report,
         blocks,
         layout
-    };
+    });
 }
