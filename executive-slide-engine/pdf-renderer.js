65d605356c3d187517c868bae38a2e24ce2856fc
zarubinscky <zarubinscky@gmail.com>
2026-07-27 18:11:05 +0300

    Update pdf-renderer.js

--- executive-slide-engine/pdf-renderer.js
+++ executive-slide-engine/pdf-renderer.js
@@ -1498,6 +1498,79 @@
     };
 }
 
+    function measureBulletBlock({
+    items,
+    font,
+    fontSize,
+    maxWidth,
+    lineHeight,
+    bulletGap = 6
+}) {
+
+    let totalHeight = 0;
+
+    const measurements = [];
+
+    for (const item of items) {
+
+        const title =
+            item.title ||
+            item.text ||
+            '';
+
+        const description =
+            item.description ||
+            '';
+
+        const titleMeasure =
+            measureText({
+                text: title,
+                font,
+                fontSize,
+                maxWidth,
+                lineHeight
+            });
+
+        let descriptionMeasure = {
+            height: 0,
+            lineCount: 0,
+            lines: []
+        };
+
+        if (description.trim()) {
+
+            descriptionMeasure =
+                measureText({
+                    text: description,
+                    font,
+                    fontSize: fontSize - 1,
+                    maxWidth,
+                    lineHeight
+                });
+
+        }
+
+        const itemHeight =
+            titleMeasure.height +
+            descriptionMeasure.height +
+            bulletGap;
+
+        totalHeight += itemHeight;
+
+        measurements.push({
+            title: titleMeasure,
+            description: descriptionMeasure,
+            height: itemHeight
+        });
+
+    }
+
+    return {
+        height: totalHeight,
+        items: measurements
+    };
+}
+
     function drawLines(
         ctx,
         options
